be5242d8b3628468a2969b039eea557f2c74664c
avrech <avrech@campus.technion.ac.il>
2020-04-16 12:28:33 +0300

    added debug message

--- separators/mccormic_cycle_separator.py
+++ separators/mccormic_cycle_separator.py
@@ -340,11 +340,19 @@ class MccormicCycleSeparator(Sepa):
         :param config: a dictionary containing the following key-value pairs.
         """
         # set scip params:
+        if self.hparams['debug']:
+            oldparams = {p: self.model.getParam(p) for p in ['objparalfac', 'dircutoffdistfac', 'efficacyfac', 'intsupportfac', 'maxcutsroot']}
         self.model.setRealParam('separating/objparalfac', config.get('objparalfac',0.1))
         self.model.setRealParam('separating/dircutoffdistfac', config.get('dircutoffdistfac',0.5))
         self.model.setRealParam('separating/efficacyfac', config.get('efficacyfac', 1))
         self.model.setRealParam('separating/intsupportfac', config.get('intsupportfac', 0.1))
         self.model.setIntParam('separating/maxcutsroot', config.get('maxcutsroot', 2000))
+        if self.hparams['debug']:
+            newparams = {p: self.model.getParam(p) for p in ['objparalfac', 'dircutoffdistfac', 'efficacyfac', 'intsupportfac', 'maxcutsroot']}
+            print('changed scip params from:')
+            print(oldparams)
+            print('to:')
+            print(newparams)
 
 
 if __name__ == "__main__":
