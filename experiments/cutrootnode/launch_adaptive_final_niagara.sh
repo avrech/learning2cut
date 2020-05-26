d53f37c53fdde39e20d54c5e969d140a2ae31fe2
Avrech Ben David <avrech@nia-login03.scinet.local>
2020-05-26 04:14:52 -0400

    launch final adaptive with graphsize 100

--- experiments/cutrootnode/launch_adaptive_final_niagara.sh
+++ experiments/cutrootnode/launch_adaptive_final_niagara.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --time=00:16:00
 #SBATCH --account=def-alodi
-#SBATCH --output=/scratch/a/alodi/avrech/cutrootnode/gsize50/adaptive-final-%j.out
+#SBATCH --output=/scratch/a/alodi/avrech/cutrootnode/gsize100/adaptive-final-%j.out
 #SBATCH --mem=4G
-
+#SBATCH --ntasks=1
 #SBATCH --mail-user=avrech@campus.tecnion.ac.il
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
@@ -12,7 +12,7 @@
 #SBATCH --mail-type=ALL
 source $HOME/login_niagara_bashrc
 cd cutrootnode
-# srun python experiment.py --log_dir /scratch/a/alodi/avrech/cutrootnode/gsize50/adaptive_final --data_dir /gpfs/fs1/home/a/alodi/avrech/learning2cut/experiments/cutrootnode/data --starting_policies_abspath /scratch/a/alodi/avrech/cutrootnode/gsize50/adaptive2/starting_policies.pkl
+# srun python experiment.py --log_dir /scratch/a/alodi/avrech/cutrootnode/gsize100/results/adaptive_final --data_dir /gpfs/fs1/home/a/alodi/avrech/learning2cut/experiments/cutrootnode/data --starting_policies_abspath /scratch/a/alodi/avrech/cutrootnode/gsize100/adaptive/starting_policies.pkl
 
-python experiment.py --log_dir $SCRATCH/cutrootnode/gsize50/adaptive-final --starting_policies_abspath $SCRATCH/cutrootnode/gsize50/adaptive-final/starting_policies.pkl
+python experiment.py --log_dir $SCRATCH/cutrootnode/gsize100/results/adaptive-final --starting_policies_abspath $SCRATCH/cutrootnode/gsize100/adaptive/starting_policies.pkl
 
