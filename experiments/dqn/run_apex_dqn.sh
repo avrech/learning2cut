c3b831e37743a8ad49df697b4c293a50357fd739
avrech <avrech@campus.tehnion.ac.il>
2020-12-12 23:43:43 +0200

    graham run script

---
+++ experiments/dqn/run_apex_dqn.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --time=3:00:00
+#SBATCH --account=def-alodi
+#SBATCH --output=apex-dqn-%j.out
+#SBATCH --mem=64G
+#SBATCH --cpus-per-task=13
+#SBATCH --job-name=apex-dqn-%j
+#SBATCH --gres=gpu:1
+#SBATCH --mail-user=avrech@campus.tecnion.ac.il
+python run_apex_dqn.py \
+  --configfile experiment_config.yaml \
+  --rootdir $SCRATCH/dqn/results/exp3 \
+  --datadir $SCRATCH/dqn/data \
+  --tags exp3 \
+  --use-gpu \
+  --num_workers 10
