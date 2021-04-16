3e8062de394c7c7355a8ae796e741af77514f9b5
avrech <avrech@campus.tehnion.ac.il>
2021-04-16 12:35:12 +0300

    generalized event hdlr

--- utils/event_hdlrs.py
+++ utils/event_hdlrs.py
@@ -2,22 +2,48 @@ from pyscipopt import Eventhdlr, SCIP_EVENTTYPE
 
 
 class DebugEvents(Eventhdlr):
+    def __init__(self, debug_events=['LPSOLVED','ROWADDEDSEPA','ROWADDEDLP','NODEBRANCHED','UBTIGHTENED','ROWDELETEDLP','ROWDELETEDSEPA']):
+        self.debug_LPSOLVED = 'LPSOLVED' in debug_events
+        self.debug_ROWADDEDSEPA = 'ROWADDEDSEPA' in debug_events
+        self.debug_ROWADDEDLP = 'ROWADDEDLP' in debug_events
+        self.debug_NODEBRANCHED = 'NODEBRANCHED' in debug_events
+        self.debug_UBTIGHTENED = 'UBTIGHTENED' in debug_events
+        self.debug_ROWDELETEDLP = 'ROWDELETEDLP' in debug_events
+        self.debug_ROWDELETEDSEPA = 'ROWDELETEDSEPA' in debug_events
 
     def eventinit(self):
         print('LPSOLVED eventinit')
-        self.model.catchEvent(SCIP_EVENTTYPE.LPSOLVED, self)
-        self.model.catchEvent(SCIP_EVENTTYPE.ROWADDEDSEPA, self)
-        self.model.catchEvent(SCIP_EVENTTYPE.ROWADDEDLP, self)
-        self.model.catchEvent(SCIP_EVENTTYPE.NODEBRANCHED, self)
-        self.model.catchEvent(SCIP_EVENTTYPE.UBTIGHTENED, self)
+        if self.debug_LPSOLVED:
+            self.model.catchEvent(SCIP_EVENTTYPE.LPSOLVED, self)
+        if self.debug_ROWADDEDSEPA:
+            self.model.catchEvent(SCIP_EVENTTYPE.ROWADDEDSEPA, self)
+        if self.debug_ROWADDEDLP:
+            self.model.catchEvent(SCIP_EVENTTYPE.ROWADDEDLP, self)
+        if self.debug_NODEBRANCHED:
+            self.model.catchEvent(SCIP_EVENTTYPE.NODEBRANCHED, self)
+        if self.debug_UBTIGHTENED:
+            self.model.catchEvent(SCIP_EVENTTYPE.UBTIGHTENED, self)
+        if self.debug_ROWDELETEDLP:
+            self.model.catchEvent(SCIP_EVENTTYPE.ROWDELETEDLP, self)
+        if self.debug_ROWDELETEDSEPA:
+            self.model.catchEvent(SCIP_EVENTTYPE.ROWDELETEDSEPA, self)
 
     def eventexit(self):
         print('LPSOLVED eventexit')
-        self.model.dropEvent(SCIP_EVENTTYPE.LPSOLVED, self)
-        self.model.dropEvent(SCIP_EVENTTYPE.ROWADDEDSEPA, self)
-        self.model.dropEvent(SCIP_EVENTTYPE.ROWADDEDLP, self)
-        self.model.dropEvent(SCIP_EVENTTYPE.NODEBRANCHED, self)
-        self.model.dropEvent(SCIP_EVENTTYPE.UBTIGHTENED, self)
+        if self.debug_LPSOLVED:
+            self.model.dropEvent(SCIP_EVENTTYPE.LPSOLVED, self)
+        if self.debug_ROWADDEDSEPA:
+            self.model.dropEvent(SCIP_EVENTTYPE.ROWADDEDSEPA, self)
+        if self.debug_ROWADDEDLP:
+            self.model.dropEvent(SCIP_EVENTTYPE.ROWADDEDLP, self)
+        if self.debug_NODEBRANCHED:
+            self.model.dropEvent(SCIP_EVENTTYPE.NODEBRANCHED, self)
+        if self.debug_UBTIGHTENED:
+            self.model.dropEvent(SCIP_EVENTTYPE.UBTIGHTENED, self)
+        if self.debug_ROWDELETEDLP:
+            self.model.dropEvent(SCIP_EVENTTYPE.ROWDELETEDLP, self)
+        if self.debug_ROWDELETEDSEPA:
+            self.model.dropEvent(SCIP_EVENTTYPE.ROWDELETEDSEPA, self)
 
     def eventexec(self, event):
         if event.getType() == SCIP_EVENTTYPE.LPSOLVED:
@@ -30,6 +56,10 @@ class DebugEvents(Eventhdlr):
             print('event - NODEBRANCHED')
         elif event.getType() == SCIP_EVENTTYPE.UBTIGHTENED:
             print('event - UBTIGHTENED')
+        elif event.getType() == SCIP_EVENTTYPE.ROWDELETEDSEPA:
+            print('event - ROWDELETEDSEPA')
+        elif event.getType() == SCIP_EVENTTYPE.ROWDELETEDLP:
+            print('event - ROWDELETEDLP')
         else:
             raise ValueError('event error')
 
