1c49ab2fad5e9553e8748d72ebf2208093cde803
avrech <avrech@campus.tehnion.ac.il>
2021-06-02 18:44:36 +0300

    fix

--- experiments/room4improvement/sbatch_niagara_scip_hparam_sweep.sh
+++ experiments/room4improvement/sbatch_niagara_scip_hparam_sweep.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --time=12:00:00
 #SBATCH --account=def-alodi
-#SBATCH --output=/scratch/a/alodi/avrech/learning2cut/tuning/
-#SBATCH --job-name=generate_dataset-%j
+#SBATCH --output=/scratch/a/alodi/avrech/learning2cut/tuning/job.out
+#SBATCH --job-name=hyperopt
 #SBATCH --mem=0
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
