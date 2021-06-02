7c75516ff80d3c0f7849cb56813577eb2448d62c
avrech <avrech@campus.tehnion.ac.il>
2021-06-02 18:42:06 +0300

    sbatch

---
+++ experiments/room4improvement/sbatch_niagara_scip_hparam_sweep.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --time=12:00:00
+#SBATCH --account=def-alodi
+#SBATCH --output=/scratch/a/alodi/avrech/learning2cut/tuning/
+#SBATCH --job-name=generate_dataset-%j
+#SBATCH --mem=0
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=40
+
+# load modules and activate virtualenv
+module load NiaEnv/2018a
+module load python
+source $HOME/server_bashrc
+source $HOME/venv/bin/activate
+
+# generate dataset
+srun python scip_hparam_tuning.py --datadir $SCRATCH/learning2cut/data --num_trials 1000
+
+
+
