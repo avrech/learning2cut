0380347b96114343bdfbf8bb61191cf99a9fcd3c
avrech <avrech@campus.tehnion.ac.il>
2021-06-04 13:04:30 +0300

    graham tuning seep sbatch

--- experiments/room4improvement/sbatch_graham_scip_hparam_sweep.sh
+++ experiments/room4improvement/sbatch_graham_scip_hparam_sweep.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 #SBATCH --time=12:00:00
 #SBATCH --account=def-alodi
-#SBATCH --output=/scratch/a/alodi/avrech/learning2cut/tuning/job.out
+#SBATCH --output=/scratch/a/alodi/avrech/learning2cut/tuning/job-%j.out
 #SBATCH --job-name=hyperopt
-#SBATCH --mem=0
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=44
+#SBATCH --mem=120G
+#SBATCH --cpus-per-task=32
 
 srun python scip_hparam_sweep.py --datadir $SCRATCH/learning2cut/data --num_trials 1000 --rootdir $SCRATCH/learning2cut/tuning
