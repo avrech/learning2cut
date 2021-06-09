0b25433b47e1e39cbaf24ca53bf49d1c8a279197
avrech <avrech@campus.tehnion.ac.il>
2021-06-09 20:43:15 +0300

    fix

--- experiments/room4improvement/run_scip_tuned_avg.py
+++ experiments/room4improvement/run_scip_tuned_avg.py
@@ -120,6 +120,11 @@ def get_data_and_configs(training=True):
             training_data['mvc'] = pickle.load(f)
         with open(f'{args.datadir}/MAXCUT/data.pkl', 'rb') as f:
             training_data['maxcut'] = pickle.load(f)
+        for d in training_data.values():
+            for k in list(d.keys()):
+                if 'valid' not in k:
+                    d.pop(k)
+
     else:
         training_data = None
     search_space = {**action_space, 'problem': ['mvc', 'maxcut']}
@@ -127,10 +132,6 @@ def get_data_and_configs(training=True):
     kv_list = []
     for k, vals in search_space.items():
         kv_list.append([(k, v) for v in vals])
-    for d in training_data.values():
-        for k in list(d.keys()):
-            if 'valid' not in k:
-                d.pop(k)
     configs = list(product(*kv_list))
     return data, training_data, configs
 
